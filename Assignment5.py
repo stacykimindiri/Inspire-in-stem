0eede1991de41914c074895c1a5dcf67b19b1803
Emmy <105636313+stacykimindiri@users.noreply.github.com>
2022-06-09 22:24:27 +0300

    Delete Assignment5.py

--- Assignment5.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#if fuction with lists
-vehicle = ["bmw","audi","toyota","mercedes","jeep"]
-#using a for loop to print a list
-for vehicle in vehicle:
-    if vehicle == "jeep" :
-        print(vehicle.upper())
