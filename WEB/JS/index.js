7a9ff71b4ebd98597c645f3be1a74d0716e1d157
Emmy <105636313+stacykimindiri@users.noreply.github.com>
2022-07-06 10:56:56 +0300

    Add files via upload
    learning on java script

---
+++ WEB/JS/index.js
@@ -0,0 +1,15 @@
+let message = "Hello world"
+console.log(message)
+//single line comment in javascript
+/*
+multi line comment in javascript
+*/
+//document.write(message) - document
+//let num_1= 78
+//let num_2= 124
+//let sum_of_nums= num_1+num_2
+//console.log(sum_of_nums)
+
+function Submit_button(){
+    alert("Button has been clicked")
+}
